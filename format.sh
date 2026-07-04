68bbe886b4bfb1a8b04b1306724d83ae701604fd
Tymon Gumienik <gumienik.tymon@gmail.com>
2026-07-04 10:18:00 +0200

    add .hpp formatting

--- format.sh
+++ format.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-clang-format -i src/*.cpp src/screens/*.cpp
+clang-format -i src/*.cpp src/screens/*.cpp src/*.hpp
--- src/app.hpp
+++ src/app.hpp
@@ -5,30 +5,30 @@
 #include "ftxui/component/screen_interactive.hpp"
 
 enum class AppStage {
-    NameInput,
-    WaitingForPeer,
-    Chatting,
+  NameInput,
+  WaitingForPeer,
+  Chatting,
 };
 
 struct AppState {
-    AppStage stage = AppStage::NameInput;
+  AppStage stage = AppStage::NameInput;
 
-    std::string username;
+  std::string username;
 };
 
 class App {
-public:
-    App();
-    void Run();
+ public:
+  App();
+  void Run();
 
-private:
-    ftxui::ScreenInteractive screen_;
-    AppState state_;
-    int tab_index_ = 0; // same order as in AppState struct
+ private:
+  ftxui::ScreenInteractive screen_;
+  AppState state_;
+  int tab_index_ = 0;  // same order as in AppState struct
 
-    ftxui::Component MakeNameInputScreen();
-    ftxui::Component MakeWaitingForPeerScreen();
-    ftxui::Component MakeChatScreen();
+  ftxui::Component MakeNameInputScreen();
+  ftxui::Component MakeWaitingForPeerScreen();
+  ftxui::Component MakeChatScreen();
 
-    void SetStage(AppStage stage);
+  void SetStage(AppStage stage);
 };
